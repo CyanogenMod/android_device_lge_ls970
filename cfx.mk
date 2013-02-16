d4283cee730918c96b349384dc96d3af4162132a
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2013-02-16 13:00:03 +0000

    fix PRODUCT_COPY_FILES

--- cfx.mk
+++ cfx.mk
@@ -15,7 +15,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Get the long list of APNs
-PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml \
+PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml \
         frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
 
 # Inherit device configuration
