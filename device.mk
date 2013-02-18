95cb07119ad39c86a4940fc0219bb7bc646dea22
synergy dev <synergye@codefi.re>
2013-02-17 19:48:16 -0800

    fix camera name

--- device.mk
+++ device.mk
@@ -49,4 +49,4 @@ PRODUCT_COPY_FILES += \
    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
 
 PRODUCT_PACKAGES += \
-        camera.geehrc4g_spr_us
+        camera.geehrc
