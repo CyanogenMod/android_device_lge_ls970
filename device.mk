c58ccda64620aefdffa124fb22814de75b1ef6ae
synergy dev <synergye@codefi.re>
2013-02-19 10:06:20 -0800

    device: fix camera.geehrc product_package

--- device.mk
+++ device.mk
@@ -48,5 +48,4 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
 
-PRODUCT_PACKAGES += \
-        camera.geehrc
+PRODUCT_PACKAGES += camera.geehrc
