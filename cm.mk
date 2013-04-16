09854a011c5a0093903bfba05021de3a41cc0adb
Jake W <xboxfanj@msn.com>
2013-04-15 19:27:44 -0500

    Fix a comment... said CFX rather than CM.

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ TARGET_SCREEN_WIDTH := 768
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CFX stuff.
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
